1f499985573b91273cb4e945eb2e10dfb47aba06
Golden Thumb <zhijunsheng@mbp2020.local>
2020-11-11 23:09:44 -0500

    drew all 6x7 circles of conn4 game board

--- Connect4/Connect4/ContentView.swift
+++ Connect4/Connect4/ContentView.swift
@@ -9,16 +9,58 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Conn4Board()
-            .stroke()
+        ZStack {
+            Conn4Board()
+                .fill(Color.white)
+            
+            Conn4Board()
+                .stroke(Color.black, lineWidth: 2)
+            
+            Conn4Grid()
+                .stroke(Color.gray)
+        }
     }
 }
 
 struct Conn4Board: Shape {
     func path(in rect: CGRect) -> Path {
+        let cellWidth: CGFloat = rect.width / 7
+        let cellHeight: CGFloat = rect.height / 6
+        let radiusRatio: CGFloat = 0.4
+        let radius: CGFloat = cellWidth > cellHeight ? radiusRatio * cellHeight : radiusRatio * cellWidth
+        
         var path = Path()
         
-        path.addArc(center: CGPoint(x: 50, y: 50), radius: 50, startAngle: .radians(0), endAngle: .radians(2 * Double.pi), clockwise: false)
+        for col in 0..<7 {
+            for row in 0..<6 {
+                let centerX: CGFloat = 0.5 * cellWidth + CGFloat(col) * cellWidth
+                let centerY: CGFloat = 0.5 * cellHeight + CGFloat(row) * cellHeight
+                
+                path.move(to: CGPoint(x: centerX + radius, y: centerY))
+                path.addArc(center: CGPoint(x: centerX, y: centerY), radius: radius, startAngle: .radians(0), endAngle: .radians(2 * Double.pi), clockwise: false)
+            }
+        }
+
+        return path
+    }
+}
+
+struct Conn4Grid: Shape {
+    func path(in rect: CGRect) -> Path {
+        let cellWidth: CGFloat = rect.width / 7
+        let cellHeight: CGFloat = rect.height / 6
+        
+        var path = Path()
+        
+        for row in 1...5 {
+            path.move(to: CGPoint(x: 0, y: CGFloat(row) * cellHeight))
+            path.addLine(to: CGPoint(x: rect.width, y: CGFloat(row) * cellHeight))
+        }
+        
+        for col in 1...6 {
+            path.move(to: CGPoint(x: CGFloat(col) * cellWidth, y: 0))
+            path.addLine(to: CGPoint(x: CGFloat(col) * cellWidth, y: rect.height))
+        }
         
         return path
     }
