fd681bba718a76b4e667632d4923db72b51f4aea
Golden Thumb <zhijunsheng@mbp2020.local>
2020-11-11 21:39:13 -0500

    created SwiftUI conn4 project for macOS

---
+++ Connect4/Connect4/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Connect4
+//
+//  Created by Golden Thumb on 2020-11-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Friend!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
