f2a64495965033fda375f67362c41cb1cf38aaf8
Golden Thumb <zhijunsheng@mbp2020.local>
2020-11-26 21:21:49 -0500

    created ConnectFourView with 6x7 circles

---
+++ Connect4/Connect4/ConnectFourView.swift
@@ -0,0 +1,30 @@
+//
+//  ConnectFourView.swift
+//  Connect4
+//
+//  Created by Golden Thumb on 2020-11-26.
+//
+
+import SwiftUI
+
+struct ConnectFourView: View {
+    var body: some View {
+        HStack {
+            ForEach (0..<7) { _ in
+                VStack {
+                    ForEach (0..<6) { _ in
+                        Circle()
+                            .stroke()
+                    }
+                }
+            }
+        }
+        .padding()
+    }
+}
+
+struct ConnectFourView_Previews: PreviewProvider {
+    static var previews: some View {
+        ConnectFourView()
+    }
+}
